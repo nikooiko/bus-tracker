b88680d0c27fad6ba0744d51d721d67612ab06b2
nick <nikooiko@inf.uth.gr>
2017-03-19 19:43:03 +0200

    [main-app] client > dashboard is now accessible only from admin.

--- server-instances/main-app/client/src/auth/store/actions.js
+++ server-instances/main-app/client/src/auth/store/actions.js
@@ -23,34 +23,48 @@ export const login = (credentials) => {
         const rememberMe = credentials.rememberMe;
         const accessToken = response.data.id;
         const isAdmin = (response.data.roles.indexOf('admin') != -1);
+        api.setAuthenticationHeader(accessToken); // add before check if admin in order to logout.
+        if (!isAdmin) {
+          // Logout because this dashboard is made only for admin.
+          dispatch(logout());
+          return Promise.reject('notAdminError');
+        }
         const user = {
           id: response.data.userId,
           username: credentials.username,
           isAdmin,
           accessToken
         };
-        api.setAuthenticationHeader(accessToken);
         dispatch(authUser(rememberMe, user));
         dispatch(routerActions.push(routeAfterAuth));
       })
-      .catch(() => {
-        const message = 'Wrong password. Try again.';
-        dispatch(showToast({
-          message,
-          status: 'critical'
-        }));
+      .catch((err) => {
+        if (err === 'notAdminError') {
+          dispatch(showToast({
+            message: 'You are not administrator. Access denied!',
+            status: 'critical'
+          }));
+        } else {
+          const message = 'Wrong password. Try again.';
+          dispatch(showToast({
+            message,
+            status: 'critical'
+          }));
+        }
       });
   }
 };
 
 export const logout = () => {
-  return (dispatch) => {
+  return (dispatch, getState) => {
     return api.post('/AppUsers/logout')
       .catch(() => null)
       .then(() => {
         api.setAuthenticationHeader(null);
-        dispatch(unauthUser());
-        dispatch(routerActions.push(routeAfterUnauth));
+        if (getState().auth.authenticated) { // if was authenticated.
+          dispatch(unauthUser());
+          dispatch(routerActions.push(routeAfterUnauth));
+        }
       });
   }
 };
