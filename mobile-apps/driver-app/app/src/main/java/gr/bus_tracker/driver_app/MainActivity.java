86b3d7086a40ead351ad371f63e320bb4c14e355
nick <nikooiko@inf.uth.gr>
2017-05-14 16:47:38 +0300

    [driver-app] Added (stub) SyncAdapter functionality.

--- mobile-apps/driver-app/app/src/main/java/gr/bus_tracker/driver_app/MainActivity.java
+++ mobile-apps/driver-app/app/src/main/java/gr/bus_tracker/driver_app/MainActivity.java
@@ -1,5 +1,8 @@
 package gr.bus_tracker.driver_app;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,13 +11,56 @@ import gr.bus_tracker.driver_app.auth.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+	// Constants
+	// The authority for the sync adapter's content provider
+	public static final String AUTHORITY = "gr.bus_tracker.driver_app.provider";
+	// An account type, in the form of a domain name
+	public static final String ACCOUNT_TYPE = "example.com";
+	// The account name
+	public static final String ACCOUNT = "dummyaccount";
+	// Instance fields
+	Account mAccount;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		// Create the dummy account
+		mAccount = CreateSyncAccount(this);
+
 		// TODO later on check if already logged in.
 		Intent intent = new Intent(this, LoginActivity.class);
 		startActivity(intent);
+	}
+
+	/**
+	 * Create a new dummy account for the sync adapter
+	 *
+	 * @param context The application context
+	 */
+	public static Account CreateSyncAccount(Context context) {
+		// Create the account type and default account
+		Account newAccount = new Account(ACCOUNT, ACCOUNT_TYPE);
+		// Get an instance of the Android account manager
+		AccountManager accountManager = (AccountManager) context.getSystemService(ACCOUNT_SERVICE);
+		/*
+		 * Add the account and account type, no password or user data
+		 * If successful, return the Account object, otherwise report an error.
+		 */
+		if (accountManager.addAccountExplicitly(newAccount, null, null)) {
+			/*
+			 * If you don't set android:syncable="true" in
+			 * in your <provider> element in the manifest,
+			 * then call context.setIsSyncable(account, AUTHORITY, 1)
+			 * here.
+			 */
+		} else {
+      /*
+       * The account exists or some other error occurred. Log this, report it,
+       * or handle it internally.
+       */
+		}
+		return newAccount;
 	}
 }
